87b53b996d769a1831a7acfaa40ad34ef602a062
James Coughlin <Jcoughlin91@gmail.com>
2022-06-16 21:48:57 -0500

    Week 2 activities

---
+++ week2/2.3.2-DataCoercionIntro.js
@@ -0,0 +1,11 @@
+// ? what should we expect to print?
+
+console.log("2" + 2); // 22
+console.log(2 + "2"); // 22
+console.log(2 + 2); // 4
+console.log("2" + "2"); // 22
+console.log(true + true); //2
+console.log(2 + true); //2true//3//
+
+console.log(true + true + "true" + "false");
+console.log(1 + 1 + "2" + "3");
