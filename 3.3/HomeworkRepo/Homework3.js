6f4621ed841f977d40098538437907369fed7901
James Coughlin <Jcoughlin91@gmail.com>
2022-06-30 18:14:53 -0500

    Updated hw3

--- 3.3/HomeworkRepo/Homework3.js
+++ 3.3/HomeworkRepo/Homework3.js
@@ -1,18 +1,30 @@
 const pizzaToppings = ["Cheese", "meat", "veggies", "Pineapple"];
 function greetCustomer() {
 let greeting = "Welcome to the Pizza Club! The toppings are";
+  //console.log(`${greeting}`)
 for (let toppings of pizzaToppings) {
-console.log(toppings)
+greeting += ` ${ toppings }`;  
 }
+  console.log(greeting);
 }
 greetCustomer();
 
-//3
-function getPizzaOrder(size, crust, ...toppings){
-console.log(`One ${size} pizza with ${crust} and ${toppings}`);  
+3
+function getPizzaOrder(size, crust, ...toppings) {
+let order = `One ${size} pizza with ${crust} and ${toppings}`;  
+  for (let topping of toppings) {
+order += `${toppings}`;
+  }
+  console.log(`${order}. coming up`);
 return [size, crust, toppings];
 } 
-getPizzaOrder("medium", "thick","Cheese", "meat", "veggies", "Pineapple");
+let customerOrder = getPizzaOrder(
+  "large", 
+  "thick",
+  "fish", 
+  "pepper",
+  "bean"
+);
 
 
 
@@ -22,9 +34,22 @@ function preparePizza(pizza) {
   return{
     size: pizza[0],
     crust: pizza[1],
-    toppings: pizza[2]
+    toppings: pizza[2] 
   }
+   
 };
 
+//5
+
+function servePizza (pizza) {
+console.log ("Order up! Here's your large thick crust pizza!")
+  size: pizza[0],
+  crust: pizza[1],
+  toppings: pizza[2]
+  return pizza 
+};
+
+//6
+function preparePizza
 
 
